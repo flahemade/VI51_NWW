6e5188f8045688d237468ac64ccdd6bfb0a12238
Felix <felix.lahemade@gmail.com>
2015-05-27 23:22:34 +0200

    First functions in Solver.

--- src/main/sarl/physics/InfluenceSolver.java
+++ src/main/sarl/physics/InfluenceSolver.java
@@ -2,14 +2,41 @@ package physics;
 
 import java.util.ArrayList;
 
+import Environment.Environment;
 import fr.utbm.info.vi51.framework.environment.Influence;
+import fr.utbm.info.vi51.framework.math.Circle2f;
+import fr.utbm.info.vi51.framework.math.Point2f;
 
 public class InfluenceSolver {
 	//Arguements
 	
 	ArrayList<Influence> influences;
+	Environment environment;
 	
 	//Methods
+	void solveConflicts(){
+		
+		ArrayList<Circle2f> influenceRadius;
+		
+		for (Influence influence : influences) {
+			
+			influenceRadius.add(this.radius);
+			
+			if(intersection){
+				
+				addWaves(a,b);
+				
+			}
+			
+		}
+	}
 	
+	void wallContact(Point2f contactPoint, Agent a){
+		// 1 = speed / frequency
+		new Agent(contactPoint,a.body.currentAmplitude,a.body.currentSpeed);
+	}
 	
+	Integer addWaves(Agent a, Agent b){
+		return a.body.currentAmplitude+b.body.currentAmplitude;
+	}
 }
