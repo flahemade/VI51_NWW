4be074a359e5a34ea2dd1c7a41bc15109b6ce817
Felix <felix.lahemade@gmail.com>
2015-05-27 20:53:55 +0200

    Created the InfluenceSolver class.

---
+++ src/main/sarl/physics/InfluenceSolver.java
@@ -0,0 +1,15 @@
+package physics;
+
+import java.util.ArrayList;
+
+import fr.utbm.info.vi51.framework.environment.Influence;
+
+public class InfluenceSolver {
+	//Arguements
+	
+	ArrayList<Influence> influences;
+	
+	//Methods
+	
+	
+}
